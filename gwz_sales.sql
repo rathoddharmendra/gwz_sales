622c734af3b6f0b7b41d6d49e468ca7ec219b746
Dharmendra Rathod <39919825+rathoddharmendra@users.noreply.github.com>
2025-06-30 09:59:10 +0200

    Added purchase cost query

---
+++ gwz_sales.sql
@@ -0,0 +1,10 @@
+SELECT 
+  sales.id AS sales_id,
+  sales.date AS sales_date,
+  sales.amount AS sales_amount,
+  customers.name AS customer_name,
+  products.name AS product_name
+FROM `gwz_sales.sales` AS sales
+JOIN `gwz_sales.customers` AS customers ON sales.customer_id = customers.id
+JOIN `gwz_sales.products` AS products ON sales.product_id = products.id
+WHERE sales.date >= '2023-01-01';
